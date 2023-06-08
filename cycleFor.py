91c06e567e8d3d922175ad8b9d41184c47940963
Cicada <hubbleadd@bk.ru>
2023-06-08 17:33:58 +0700

    Add cycles example

---
+++ cycleFor.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+#
+# Trivial example for cycle 'for' in Python3
+#
+#-----------------------------------------------------------------------------
+
+n = int(input("Input the number: "))
+
+print("Begin cycle...")
+
+for i in range(0, n):
+    print("iteration number:", i)
+
+print("End cycle", '\n\n')
+
+
---
+++ cycleWhile.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+#
+# Trivial example for cycle 'while' in Python3
+#
+#-----------------------------------------------------------------------------
+
+n = int(input("Input n: "))
+
+while n > 0:
+    print("n is", n)
+    n = n - 1
+
+# Lets do is forever and count iteration!!!
+n = 0
+while True:
+    print("Where's the end???")
+    print("Iteration ", n)
+    n = n + 1
